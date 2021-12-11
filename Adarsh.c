a62b62efcda2fbefde865070956738d0fe0808d2
AdarshP18 <95960107+AdarshP18@users.noreply.github.com>
2021-12-11 16:37:32 +0530

    Create Adarsh.c

---
+++ Adarsh.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{ 
+ clrscr();
+ int x;
+ int y;
+ printf("Enter the first number: ");
+ scanf("%d",&x);
+ 
+ printf("Enter the second number: ");
+ scanf("%d,&y);
+ int z=x+y
+ printf("Sum of two numbers is: %d",z);
+ getch();
+}
